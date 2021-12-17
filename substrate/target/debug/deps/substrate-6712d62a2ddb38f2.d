dfa422ca1f5e2b492465557a63951eda49f1bafa
Sagar <41965306+svgvr8@users.noreply.github.com>
2021-12-17 18:04:42 +0530

    node

---
+++ substrate/target/debug/deps/substrate-6712d62a2ddb38f2.d
@@ -0,0 +1,5 @@
+/mnt/d/health-hero/web3-decentarlized-api/substrate/target/debug/deps/substrate-6712d62a2ddb38f2: src/main.rs
+
+/mnt/d/health-hero/web3-decentarlized-api/substrate/target/debug/deps/substrate-6712d62a2ddb38f2.d: src/main.rs
+
+src/main.rs:
---
+++ substrate/target/debug/substrate.d
@@ -0,0 +1,1 @@
+/mnt/d/health-hero/web3-decentarlized-api/substrate/target/debug/substrate: /mnt/d/health-hero/web3-decentarlized-api/substrate/src/main.rs
